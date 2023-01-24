0859549af8544df19469a30e8444777f0bd5da7e
Jadson Gomes de Silva <igrejaemidias@gmail.com>
2023-01-23 21:20:50 -0300

    organizando elementos e melhorando css

---
+++ teste.php
@@ -0,0 +1,31 @@
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<link rel="stylesheet" type="text/css" href="src/estilo.css" />
+<meta charset="utf-8" />
+    <title>Desenhando na canvas</title>
+</head>
+
+<body>
+<p>
+<h1>Leia atentamente o contrato abaixo:</h1>
+
+
+</p>
+    <iframe src="exemplo.pdf" id="iframepdf" height="350"></iframe>
+<p>
+    Caso concorde com o contrato assine no quadro abaixo e digite o código recebido via SMS no número: 41 99999-9999:    <input type="text" maxlength="5">
+</p>
+Enviar contrato assinado por e-mail: Digite seu e-mail: <input type="email" name="emial" id="email">
+
+    <canvas id="quadro" style=></canvas>
+<button id="limparAssinatura" style="background-color: red; border:none; align-items: center; align-content: center;">Limpar</button>
+<button id="salvar" style="background-color: aqua; border:none;align-items: center; align-content: center;">Lí e concordo</button>
+
+</p>
+
+</body>
+<script src="src/assinatura.js">
+</script>
+
+</html>
