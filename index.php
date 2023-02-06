7ceb4e7899b03eef4d796a1ad7e60920e927a342
wesleylourenco1 <wesleylourenco1@gmail.com>
2023-02-06 20:52:27 -0300

    adicionado bootstrap css/js

--- index.php
+++ index.php
@@ -1,30 +1,44 @@
 <html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<link rel="stylesheet" type="text/css" href="src/estilo.css" />
-<meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="src/estilo.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="utf-8" />
     <title>Desenhando na canvas</title>
 </head>
 
 <body>
-<p>
-<h1>Leia atentamente o contrato abaixo:</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>Leia atentamente o contrato abaixo:</h2>
+
+            </div>
+
+        </div>
+
+        <iframe src="exemplo.pdf" id="iframepdf" height="350"></iframe>
+        <div class="row alert alert-info">
+            <div class="col-sm-8 small" role="alert">
+                Caso concorde com o contrato assine no quadro abaixo e digite o código recebido via SMS no número: 41 99999-9999:
+            </div>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" maxlength="5">
+            </div>
+        </div>
 
 
-</p>
-    <iframe src="exemplo.pdf" id="iframepdf" height="350"></iframe>
-<p>
-    Caso concorde com o contrato assine no quadro abaixo e digite o código recebido via SMS no número: 41 99999-9999:    <input type="text" maxlength="5">
-</p>
-Enviar contrato assinado por e-mail: Digite seu e-mail: <input type="email" name="emial" id="email">
+        Enviar contrato assinado por e-mail: Digite seu e-mail: <input type="email" class="form-control" name="emial" id="email">
 
-    <canvas id="quadro" style=></canvas>
-<button id="limparAssinatura" style="background-color: red; border:none; align-items: center; align-content: center;">Limpar</button>
-<button id="salvar" style="background-color: aqua; border:none;align-items: center; align-content: center;">Lí e concordo</button>
+        <canvas id="quadro" style=></canvas>
+        <button id="limparAssinatura" style="background-color: red; border:none; align-items: center; align-content: center;">Limpar</button>
+        <button id="salvar" style="background-color: aqua; border:none;align-items: center; align-content: center;">Lí e concordo</button>
 
-</p>
-
+        </p>
+    </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <script src="src/assinatura.js">
 </script>
 
